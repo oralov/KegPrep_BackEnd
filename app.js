01a79efe7d52376e7ba9700a566df7acc722b4ac
oralov <50965138+oralov@users.noreply.github.com>
2023-02-17 20:54:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 const { MongoClient} = require('mongodb');
 const uri = "mongodb+srv://Admin:27071995aA@cluster0.tdenifs.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const cors = require('cors')
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cors());
+
+app.get("/", (req, res) =>{
+  res.sendFile('public/search.html', {root: __dirname });
+})
 
 
 app.post('/recipes', (req, res) => {
@@ -12,7 +18,7 @@ app.post('/recipes', (req, res) => {
    for(let i = 0; i < req.body.ing.length; i++){
      arr.push({Name: req.body.ing[i], Amount: req.body.amount[i], Measure: req.body.measure[i]});
    }
-   let recipe = { Name: req.body.name.toLowerCase(),
+   let recipe = { Name: req.body.name,
                   Ingredients: arr,
                   Steps: req.body.step,
                   Note: req.body.note,
@@ -31,13 +37,23 @@ client.connect(err => {
           });
 })
 
+app.get("/recipes", (req, res) => {
+  client.connect(err => {
+    const collection = client.db("KegPrep").collection("recipes");
+    collection.find({}).toArray(function(err, result) {
+      if(err) throw err;
+      res.send(result);
+    })
+  })
+})
+
 app.get("/recipes/:recipeName", (req,res) =>{
   client.connect(err => {
     const collection = client.db("KegPrep").collection("recipes");
-    collection.findOne({Name: `${req.params.recipeName}`}, function(err, result) {
+    collection.findOne({Name: {$regex: req.params.recipeName.toLowerCase()}}, function(err, result) {
       
-      if (err) throw err;
-     res.send(
+     if (err) throw err;
+     /*res.send(
       `<p>${result.Name}</p>
       <div>
        ${
@@ -57,6 +73,10 @@ app.get("/recipes/:recipeName", (req,res) =>{
 
       `
      )
+      */
+      res.send(result);
+     
+      
       client.close();
     });
   });
